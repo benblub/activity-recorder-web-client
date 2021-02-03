13d0f7236da9ee84990f180f4e80337dd49ae90d
Benjamin Knecht <knecht.benjamin@yahoo.de>
2021-02-03 23:46:07 +0100

    show validation errors ontop or inputPath

--- src/views/Activity/CreateActivity.vue
+++ src/views/Activity/CreateActivity.vue
@@ -1,18 +1,41 @@
 <template>
     <div class="activityCreate">
         <h1>Create Activity</h1>
+
+        <div v-if="errors.length > 0" class="alert alert-danger" role="alert">
+            <p v-for="error in errors" :key="error">{{ error }}</p>
+        </div>
+
         <form v-on:submit.prevent>
             <div class="form-group">
                 <label for="date">Date</label>
-                <input v-model="date" type="date" class="form-control" id="date" placeholder="date">
+                <input
+                        v-model="date"
+                        type="date"
+                        class="form-control"
+                        v-bind:class="{'is-invalid': dateError}"
+                        id="date"
+                        placeholder="date"
+                >
             </div>
             <div class="form-group">
                 <label for="time">Time</label>
-                <input v-model="time" type="number" class="form-control" id="time" placeholder="time">
+                <input v-model="time" type="number"
+                       class="form-control"
+                       v-bind:class="{'is-invalid': timeError}"
+                       id="time"
+                       placeholder="time"
+                >
             </div>
             <div class="form-group">
                 <label for="description">Description</label>
-                <textarea v-model="description" class="form-control" id="description" rows="3"></textarea>
+                <textarea v-model="description"
+                          class="form-control"
+                          v-bind:class="{'is-invalid': descriptionError}"
+                          id="description"
+                          rows="3">
+                </textarea>
+                <small style="color: red">{{ descriptionError }}</small>
             </div>
 
             <button
@@ -31,7 +54,11 @@
             return {
                 date: null,
                 time: null,
-                description: null
+                description: null,
+                descriptionError: null,
+                timeError: null,
+                dateError: null,
+                errors: []
             }
         },
         methods: {
@@ -48,8 +75,23 @@
                     console.log(e.message)
                     console.log(e.response)
                     console.log(e.response.data)
-                }
 
+
+                    if (e.response.data.violations) {
+                        for(let i in e.response.data.violations) {
+                            // map on top or on form field path
+                            if (e.response.data.violations[i].propertyPath == '') {
+                                this.errors.push(e.response.data.violations[i].message)
+                            } else if (e.response.data.violations[i].propertyPath == 'description') {
+                                this.descriptionError = e.response.data.violations[i].message
+                            } else if (e.response.data.violations[i].propertyPath == 'performendTime') {
+                                this.timeError = e.response.data.violations[i].message
+                            } else if (e.response.data.violations[i].propertyPath == 'activityDate') {
+                                this.dateError = e.response.data.violations[i].message
+                            }
+                        }
+                    }
+                }
                 console.log(response)
             }
         }
